5ed0f52af802012e3caf510778b14cbac292af45
olesigilai <ole_sigilai@yahoo.com>
2021-01-30 10:46:55 +0300

    quotes class added

---
+++ src/app/quote.ts
@@ -0,0 +1,15 @@
+export class Quote {
+    
+    constructor(
+        id: number,
+        quote: string,
+        author: string,
+        date: Date,
+        upvotes: number,
+        downvotes: number
+    ){
+
+    }
+  
+
+}
