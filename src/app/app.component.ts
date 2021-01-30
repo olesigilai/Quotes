7f5c3ed6f7da8a2db02378d8ccffe6b1bc50496b
olesigilai <ole_sigilai@yahoo.com>
2021-01-30 11:45:29 +0300

    quotes component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { identifierModuleUrl } from '@angular/compiler';
 import { Component } from '@angular/core';
-
+import { from } from 'rxjs';
+import { Quote } from './quote'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quote = 'Enjoy our Website adventure'
+  
 }
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,14 +1,15 @@
 export class Quote {
-    
+    showContent: boolean
     constructor(
         id: number,
         quote: string,
         author: string,
+        // name:string,
         date: Date,
         upvotes: number,
         downvotes: number
     ){
-
+this.showContent = false;
     }
   
 
