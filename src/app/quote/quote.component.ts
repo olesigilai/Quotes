b073886e480c1e2e7eeb68aa818a86bc801a8592
olesigilai <ole_sigilai@yahoo.com>
2021-02-02 12:32:58 +0300

    dates modified

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,8 +9,8 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   Quotes: Quote[] = [
-    new Quote(0, "Life is not a straight line, treat everyone equally.", "Akoko", "Gideon", new Date(2021, 1, 2), 0, 0),
-    new Quote(1, "Live a life of your own, never trust other people's ways.", "Balotelli", "Kiplagat", new Date(2021, 1, 2), 0, 0),
+    new Quote(0, "Life is not a straight line, treat everyone equally.", "Akoko", "Gideon", new Date(2021, 1, 1), 0, 0),
+    new Quote(1, "Live a life of your own, never trust other people's ways.", "Balotelli", "Kiplagat", new Date(2021, 0, 1), 0, 0),
     new Quote(2, "Train yourself to be satisfied with what you have. Otherwise,You will die young.", "The Chariot Boy", "Bad Boy", new Date(2021, 1, 1), 0, 0),
     new Quote(3, "Train you child of the right way to go  and he won't forget this old age .", "Generali Kwack", "Jacky Daniels", new Date(2021, 1, 1), 0, 0),
 
